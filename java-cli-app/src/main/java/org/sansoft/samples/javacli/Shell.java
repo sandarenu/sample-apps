35f591e4810fb8b868344fec0971bbc0206040b2
Chathurika Sandarenu <sandarenu@gmail.com>
2011-11-15 06:52:56 +0530

    added sample code on how to use jline

---
+++ java-cli-app/src/main/java/org/sansoft/samples/javacli/Shell.java
@@ -0,0 +1,80 @@
+package org.sansoft.samples.javacli;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import jline.ArgumentCompletor;
+import jline.ConsoleReader;
+import jline.SimpleCompletor;
+
+/**
+ * Sample application to show how jLine can be used.
+ *
+ * @author sandarenu
+ *
+ */
+public class Shell {
+	private String[] commandsList;
+
+	public void init() {
+		commandsList = new String[] { "help", "action1", "action2", "exit" };
+	}
+
+	public void run() throws IOException {
+		printWelcomeMessage();
+		ConsoleReader reader = new ConsoleReader();
+		reader.setBellEnabled(false);
+		List completors = new LinkedList();
+
+		completors.add(new SimpleCompletor(commandsList));
+		reader.addCompletor(new ArgumentCompletor(completors));
+
+		String line;
+		PrintWriter out = new PrintWriter(System.out);
+
+		while ((line = readLine(reader, "")) != null) {
+			if ("help".equals(line)) {
+				printHelp();
+			} else if ("action1".equals(line)) {
+				System.out.println("You have selection action1");
+			} else if ("action2".equals(line)) {
+				System.out.println("You have selection action2");
+			} else if ("exit".equals(line)) {
+				System.out.println("Exiting application");
+				return;
+			} else {
+				System.out
+						.println("Invalid command, For assistance press TAB or type \"help\" then hit ENTER.");
+			}
+			out.flush();
+		}
+	}
+
+	private void printWelcomeMessage() {
+		System.out
+				.println("Welcome to jLine Sample App. For assistance press TAB or type \"help\" then hit ENTER.");
+
+	}
+
+	private void printHelp() {
+		System.out.println("help		- Show help");
+		System.out.println("action1		- Execute action1");
+		System.out.println("action2		- Execute action2");
+		System.out.println("exit        - Exit the app");
+
+	}
+
+	private String readLine(ConsoleReader reader, String promtMessage)
+			throws IOException {
+		String line = reader.readLine(promtMessage + "\nshell> ");
+		return line.trim();
+	}
+
+	public static void main(String[] args) throws IOException {
+		Shell shell = new Shell();
+		shell.init();
+		shell.run();
+	}
+}
